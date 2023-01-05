79813198d9e159b357fedb52365c5b305f112687
Teddy <buttguy@kcmotors.co.kr>
2023-01-05 15:53:08 +0900

    230105

---
+++ src/app_httpd.cpp
@@ -0,0 +1,352 @@
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include "esp_camera.h"
+#include "img_converters.h"
+#include "fb_gfx.h"
+#include "driver/ledc.h"
+#include "sdkconfig.h"
+#include "Arduino.h"
+
+#define MOTOR_1_PIN_1 14
+#define MOTOR_1_PIN_2 15
+#define MOTOR_2_PIN_1 13
+#define MOTOR_2_PIN_2 12
+
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+int led_duty = 0;
+bool isStreaming = false;
+#ifdef CONFIG_LED_LEDC_LOW_SPEED_MODE
+#define CONFIG_LED_LEDC_SPEED_MODE LEDC_LOW_SPEED_MODE
+#else
+#define CONFIG_LED_LEDC_SPEED_MODE LEDC_HIGH_SPEED_MODE
+#endif
+#endif
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\nX-Timestamp: %d.%06d\r\n\r\n";
+
+httpd_handle_t stream_httpd = NULL;
+httpd_handle_t camera_httpd = NULL;
+
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<html>
+  <head>
+    <title>ESP32 RC Car</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
+      table { margin-left: auto; margin-right: auto; }
+      td { padding: 8 px; }
+      .button {
+        background-color: #2f4468;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 18px;
+        margin: 6px 3px;
+        cursor: pointer;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+      }
+      img {  width: auto ;
+        max-width: 100% ;
+        height: auto ; 
+      }
+    </style>
+  </head>
+  <body>
+    <h1>ESP32 RC Car</h1>
+    <img src="" id="photo" >
+    <table>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('forward');" ontouchstart="toggleCheckbox('forward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Forward</button></td></tr>
+      <tr><td align="center"><button class="button" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Left</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('stop');" ontouchstart="toggleCheckbox('stop');">Stop</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Right</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('backward');" ontouchstart="toggleCheckbox('backward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Backward</button></td></tr>                   
+    </table>
+   <script>
+   function toggleCheckbox(x) {
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/action?go=" + x, true);
+     xhr.send();
+   }
+   window.onload = document.getElementById("photo").src = window.location.href.slice(0, -1) + ":81/stream";
+   var deg = 0;
+   function rotateLeft() {
+     deg -= 90;
+     if ( deg <0 ) deg = 360;
+     console.log("Rotate image to left");
+     document.getElementById("photo").style.transform = 'rotate(' + deg + 'deg)';
+   }
+   function rotateRight() {
+     deg += 90;
+     if ( deg > 360 ) deg = 0;
+     console.log("Rotate image to right");
+     document.getElementById("photo").style.transform = 'rotate(' + deg + 'deg)';
+   }
+  </script>
+  </body>
+</html>
+)rawliteral";
+
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+void enable_led(bool en)
+{ // Turn LED On or Off
+    int duty = en ? led_duty : 0;
+    if (en && isStreaming && (led_duty > CONFIG_LED_MAX_INTENSITY))
+    {
+        duty = CONFIG_LED_MAX_INTENSITY;
+    }
+    ledc_set_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL, duty);
+    ledc_update_duty(CONFIG_LED_LEDC_SPEED_MODE, CONFIG_LED_LEDC_CHANNEL);
+    Serial.println("\r\nSet LED intensity to %d", duty);
+}
+#endif
+
+static esp_err_t stream_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL;
+    struct timeval _timestamp;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t *_jpg_buf = NULL;
+    char *part_buf[128];
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if (res != ESP_OK)
+    {
+        return res;
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    httpd_resp_set_hdr(req, "X-Framerate", "60");
+
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+    enable_led(true);
+    isStreaming = true;
+#endif
+
+    while (true)
+    {
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            Serial.println("\r\nCamera capture failed");
+            res = ESP_FAIL;
+        }
+        else
+        {
+            _timestamp.tv_sec = fb->timestamp.tv_sec;
+            _timestamp.tv_usec = fb->timestamp.tv_usec;
+//            if (fb->width > 400)
+//            {
+                if (fb->format != PIXFORMAT_JPEG)
+                {
+                    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                    if (!jpeg_converted)
+                    {
+                        Serial.println("\r\nJPEG compression failed");
+                        res = ESP_FAIL;
+                    }
+                }
+                else
+                {
+                    _jpg_buf_len = fb->len;
+                    _jpg_buf = fb->buf;
+                }
+//            }
+        }
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if (res == ESP_OK)
+        {
+            size_t hlen = snprintf((char *)part_buf, 128, _STREAM_PART, _jpg_buf_len, _timestamp.tv_sec, _timestamp.tv_usec);
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if (fb)
+        {
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        }
+        else if (_jpg_buf)
+        {
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if (res != ESP_OK)
+        {
+            Serial.printf("\r\nsend frame failed failed");
+            break;
+        }
+    }
+
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+    isStreaming = false;
+    enable_led(false);
+#endif
+
+    return res;
+}
+
+static esp_err_t parse_get(httpd_req_t *req, char **obuf)
+{
+    char *buf = NULL;
+    size_t buf_len = 0;
+
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = (char *)malloc(buf_len);
+        if (!buf) {
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            *obuf = buf;
+            return ESP_OK;
+        }
+        free(buf);
+    }
+    httpd_resp_send_404(req);
+    return ESP_FAIL;
+}
+
+static esp_err_t cmd_handler(httpd_req_t *req)
+{
+    char *buf = NULL;
+    char variable[32];
+
+    if (parse_get(req, &buf) != ESP_OK) {
+        return ESP_FAIL;
+    }
+    if (httpd_query_key_value(buf, "go", variable, sizeof(variable)) != ESP_OK) {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
+    free(buf);
+
+    sensor_t *s = esp_camera_sensor_get();
+    int res = 0;
+
+    if(!strcmp(variable, "forward")) {
+      Serial.println("Forward");
+      digitalWrite(MOTOR_1_PIN_1, 1);
+      digitalWrite(MOTOR_1_PIN_2, 0);
+      digitalWrite(MOTOR_2_PIN_1, 1);
+      digitalWrite(MOTOR_2_PIN_2, 0);
+    }
+    else if(!strcmp(variable, "left")) {
+      Serial.println("Left");
+      digitalWrite(MOTOR_1_PIN_1, 0);
+      digitalWrite(MOTOR_1_PIN_2, 1);
+      digitalWrite(MOTOR_2_PIN_1, 1);
+      digitalWrite(MOTOR_2_PIN_2, 0);
+    }
+    else if(!strcmp(variable, "right")) {
+      Serial.println("Right");
+      digitalWrite(MOTOR_1_PIN_1, 1);
+      digitalWrite(MOTOR_1_PIN_2, 0);
+      digitalWrite(MOTOR_2_PIN_1, 0);
+      digitalWrite(MOTOR_2_PIN_2, 1);
+    }
+    else if(!strcmp(variable, "backward")) {
+      Serial.println("Backward");
+      digitalWrite(MOTOR_1_PIN_1, 0);
+      digitalWrite(MOTOR_1_PIN_2, 1);
+      digitalWrite(MOTOR_2_PIN_1, 0);
+      digitalWrite(MOTOR_2_PIN_2, 1);
+    }
+    else if(!strcmp(variable, "stop")) {
+      Serial.println("Stop");
+      digitalWrite(MOTOR_1_PIN_1, 0);
+      digitalWrite(MOTOR_1_PIN_2, 0);
+      digitalWrite(MOTOR_2_PIN_1, 0);
+      digitalWrite(MOTOR_2_PIN_2, 0);
+    }
+//#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+//    else if (!strcmp(variable, "led_intensity")) {
+//        led_duty = val;
+//        if (isStreaming)
+//            enable_led(true);
+//    }
+//#endif
+    else {
+        Serial.printf("Unknown command: %s", variable);
+        res = -1;
+    }
+
+    if (res < 0) {
+        return httpd_resp_send_500(req);
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, NULL, 0);
+}
+
+static esp_err_t index_handler(httpd_req_t *req)
+{
+    httpd_resp_set_type(req, "text/html");
+    sensor_t *s = esp_camera_sensor_get();
+    if (s != NULL) {
+        return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
+    } else {
+        Serial.printf("\r\nCamera sensor not found");
+        return httpd_resp_send_500(req);
+    }
+}
+
+void startCameraServer()
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.max_uri_handlers = 16;
+
+    httpd_uri_t index_uri = {
+        .uri = "/",
+        .method = HTTP_GET,
+        .handler = index_handler,
+        .user_ctx = NULL};
+
+    httpd_uri_t cmd_uri = {
+        .uri = "/action",
+        .method = HTTP_GET,
+        .handler = cmd_handler,
+        .user_ctx = NULL};
+
+    httpd_uri_t stream_uri = {
+        .uri = "/stream",
+        .method = HTTP_GET,
+        .handler = stream_handler,
+        .user_ctx = NULL};
+
+    Serial.printf("\r\nStarting web server on port: '%d'", config.server_port);
+    if (httpd_start(&camera_httpd, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(camera_httpd, &index_uri);
+        httpd_register_uri_handler(camera_httpd, &cmd_uri);
+    }
+
+    config.server_port += 1;
+    config.ctrl_port += 1;
+    Serial.printf("\r\nStarting stream server on port: '%d'", config.server_port);
+    if (httpd_start(&stream_httpd, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(stream_httpd, &stream_uri);
+    }
+}
